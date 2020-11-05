2233925b3eb73c5075d88a1f2d2fa5561be67712
kimxons <kitongameshack9@gmail.com>
2020-11-05 14:38:14 +0300

    smart contracts

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,6 +2,7 @@ var MyContract = artifacts.require("MyContract");
 var Consumer = artifacts.require("Consumer");
 var Distributor = artifacts.require("Distributor");
 var Farmer = artifacts.require("Farmer");
+var Roles = artifacts.require("Roles");
 
 module.exports  = function(deployer)
 {
