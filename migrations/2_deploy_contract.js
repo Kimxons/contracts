4a1f14442ee0e9688253304cf762c8bde3ed80d9
kimxons <kitongameshack9@gmail.com>
2020-11-05 15:31:44 +0300

    truffle

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,10 +1,6 @@
-var MyContract = artifacts.require("MyContract");
-var Consumer = artifacts.require("Consumer");
-var Distributor = artifacts.require("Distributor");
-var Farmer = artifacts.require("Farmer");
-var Roles = artifacts.require("Roles");
+var MyContract = artifacts.require("MyContract"); //this is the main smart contract
 
 module.exports  = function(deployer)
 {
-    deployer.deploy(MyContract)
+    deployer.deploy(MyContract);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,25 @@
 const path = require("path");
 
 module.exports = {
+	contracts_directory: path.join(__dirname, "contracts"),
+	migratins_directory: path.join(__dirname, "migrations"),
 	contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 	networks: {
 		development: {
 			host: "127.0.0.1",
 			port: 9545,
-			network_id: "*" //to match ay network id
-		}
+			network_id: "*", //to match ay network id
+			websockets: true
+		},
+		live: {
+			host: "*", 
+			port: 8080,
+			network_id: 1
+		},
+		etherscan: {
+			apikey: "AW9BDBGDU3KF469TM1SG25D7XNWJTF98I5"
+		},
+		sourceFetchers: ["sourcify", "etherscan"]
 	}
 };
 
